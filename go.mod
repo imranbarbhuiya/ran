a15a82c7b022c2729adf4cfe8b837793cd3ed82b
parbez <imranbarbhuiya.fsd@gmail.com>
2024-07-26 16:17:26 +0530

    feat: add history and cursor support to repl

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module ran
 
 go 1.22.5
+
+require (
+	github.com/chzyer/readline v1.5.1 // indirect
+	golang.org/x/sys v0.0.0-20220310020820-b874c991c1a5 // indirect
+)
--- repl/repl.go
+++ repl/repl.go
@@ -1,33 +1,49 @@
 package repl
 
 import (
-	"bufio"
 	"fmt"
 	"io"
 	"ran/evaluator"
 	"ran/lexer"
 	"ran/object"
 	"ran/parser"
+
+	"github.com/chzyer/readline"
 )
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
-	scanner := bufio.NewScanner(in)
+	rl, err := readline.New(PROMPT)
+	if err != nil {
+		panic(err)
+	}
+	defer rl.Close()
+
 	env := object.NewEnvironment()
 	macroEnv := object.NewEnvironment()
 
 	for {
-		fmt.Fprint(out, PROMPT)
-		scanned := scanner.Scan()
-		if !scanned {
-			return
+		line, err := rl.Readline()
+		if err == readline.ErrInterrupt {
+			if len(line) == 0 {
+				break
+			} else {
+				continue
+			}
+		} else if err == io.EOF {
+			break
 		}
-		line := scanner.Text()
+
 		if line == "clear" {
 			fmt.Fprintf(out, "\033[H\033[2J")
 			continue
 		}
+
+		if line == "exit" {
+			break
+		}
+
 		l := lexer.New(line)
 		p := parser.New(l)
 		program := p.ParseProgram()
@@ -46,6 +62,7 @@ func Start(in io.Reader, out io.Writer) {
 		}
 	}
 }
+
 func PrintParserErrors(out io.Writer, errors []string) {
 	io.WriteString(out, "Woops! We ran into some errors here!\n")
 	io.WriteString(out, " parser errors:\n")
